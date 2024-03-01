027cd0aeac38f73f67c62c35af2f9cdd31bde700
hiroi9361 <hiroi9361@gmail.com>
2024-03-02 00:14:41 +0900

    2024/03/02

---
+++ nagoyameshi_hi/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+-- housesテーブル
+INSERT IGNORE INTO stores (id, name, image_name, description, price, capacity, postal_code, address, phone_number) VALUES (1, 'SAMURAIの宿', 'house01.jpg', '最寄り駅から徒歩10分。自然豊かで閑静な場所にあります。長期滞在も可能です。', 6000, 2, '073-0145', '北海道砂川市西五条南X-XX-XX', '012-345-678');
