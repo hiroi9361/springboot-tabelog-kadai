a5ad138b08296d7b04819cac1036f77ba2bcf8a1
hiroi9361 <hiroi9361@gmail.com>
2024-03-06 04:10:34 +0900

    2024/03/05

--- nagoyameshi_hi/src/main/java/com/example/demo/controller/HouseController.java
+++ nagoyameshi_hi/src/main/java/com/example/demo/controller/HouseController.java
@@ -7,10 +7,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.House;
+import com.example.demo.form.ReservationInputForm;
 import com.example.demo.repository.HouseRepository;
 
 @Controller
@@ -32,7 +34,6 @@ public class HouseController {
 		Page<House> housePage;
 
 		if (keyword != null && !keyword.isEmpty()) {
-
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
 						"%" + keyword + "%", pageable);
@@ -41,21 +42,18 @@ public class HouseController {
 						"%" + keyword + "%", pageable);
 			}
 		} else if (area != null && !area.isEmpty()) {
-
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
 			} else {
 				housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
 			}
 		} else if (price != null) {
-
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
 			} else {
 				housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
 			}
 		} else {
-
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
 			} else {
@@ -70,5 +68,15 @@ public class HouseController {
 		model.addAttribute("order", order);
 
 		return "houses/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
+		House house = houseRepository.getReferenceById(id);
+
+		model.addAttribute("house", house);
+		model.addAttribute("reservationInputForm", new ReservationInputForm());
+
+		return "houses/show";
 	}
 }
